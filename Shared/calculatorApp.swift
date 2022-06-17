9bd093774620cc846fe21782939baa4b020002ed
Hitesh Goel <hiteshgoel426@gmail.com>
2022-06-17 21:29:01 +0530

    Initial Commit

---
+++ Shared/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  Shared
+//
+//  Created by Hitesh Goel on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
